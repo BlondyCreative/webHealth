fbd9b83d190a1b3a786426613bf65e6caccc3abb
Nicolle <nicolegonzaleslearn@gmail.com>
2025-12-17 12:28:10 -0400

    Better the links href

--- backend/jss/script.js
+++ backend/jss/script.js
@@ -4,7 +4,7 @@ import { toggleBookmarkIcon } from "/appHealth/backend/control/viewPage.js";
 import * as model from "/appHealth/backend/model/control.js";
 import * as controlPage from "/appHealth/backend/control/controlPage.js";
 import { syncBookmarkIcons } from "/appHealth/backend/control/viewPage.js";
-
+import { sendSms } from "/appHealth/backend/model/sendSms.js";
 const controlBookmarks = function (list, btn) {
   const action = model.toggleBookmarks(list);
   toggleBookmarkIcon(btn, action);
@@ -17,7 +17,10 @@ const controlBookmarks = function (list, btn) {
 
   model.btnPatient();
 
-};
+  document.getElementById("contactForm").addEventListener("submit-btn", (e) => 
+    { e.preventDefault();
+    sendSms(e.target); }
+)};
 
 const init = function (bookmark) {
   controlPage.init();
--- router.php
+++ router.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $page = $_GET['page'] ?? 'login';
-$permitidas = ['login', 'register', 'results', 'sect1', 'sect2', 'sect3'];
+$permitidas = ['login', 'register', 'results', 'sect1', 'sect2', 'sect3', 'about', 'contactUs'];
 
 if (!in_array($page, $permitidas)) {
     http_response_code(404);
